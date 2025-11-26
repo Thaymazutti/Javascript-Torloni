e330ea41c1b3e31b6bc62c94029ae9fb2e2d5331
THAYSSA MAZUTTI DA CRUZ <46209333842@imaccommand021.local>
2025-11-26 16:49:45 -0300

    iniciado os exercícios sobre o laço for

---
+++ Estrutura de repetição/for.js
@@ -0,0 +1,21 @@
+// sitaxe = escrita do código 
+
+// for - laço de repetição = loop
+// um laço de de repetição tem voltas
+
+// laco contado - quando sabemos quantas voltas
+// laço condicional - quando irá fazer as voltas se a condição for verdadeira
+
+//for ([inicialização]; [condição]; [incremento];){
+
+//}
+// a variavel i vale 0 ou seja essa variavel armazena as voltas
+// a condição verifica o numero que o loop esta
+for(let i = 0; i <= 9; i = i + 1){
+    console.log("A volta que for está é: " + 1);
+    }
+
+    //utilize o for para exibir do 1 ao 10
+    for(let i = 1; i <= 10; i++){
+        console.log(i)
+    }
